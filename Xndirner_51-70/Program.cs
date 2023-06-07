da06c5cc92362bcc679b809d47ea7b25cccebdb4
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-07 18:15:26 +0400

    Add project files.

---
+++ Xndirner_51-70/Program.cs
@@ -0,0 +1,464 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndirner_51_70
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //51
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //bool t = false;
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (c == a + b)
+            //{
+            //    t = true;
+            //}
+            //Console.WriteLine(t);
+
+            //52
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //bool t = false;
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (a == b || a == c || b == c)
+            //{
+            //    t = true;
+            //}
+            //Console.WriteLine(t);
+
+            //53
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //Console.Write("mutqagrel k: ");
+            //int k = int.Parse(Console.ReadLine());
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (n > k)
+            //{
+            //    Console.WriteLine((double)n / (a + b + c));
+            //}else
+            //    Console.WriteLine((double)c / n);
+
+            //54
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //int max = 0;
+
+            //if (a > b && a > c)
+            //{
+            //    max = a;
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    max = b;
+            //}
+            //else
+            //    max = c;
+
+            //Console.WriteLine($"Max: {max}");
+
+            //55
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //int min = 0;
+
+            //if (a < b && a < c)
+            //{
+            //    min = a;
+            //}
+            //else if (b < a && b < c)
+            //{
+            //    min = b;
+            //}
+            //else
+            //    min = c;
+
+            //Console.WriteLine($"Min: {min}");
+
+            //56
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (c > b)
+            //    Console.WriteLine((double)(a+ b +c) / n);
+            //else
+            //    Console.WriteLine(n);
+
+            //57
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100 && n % 10 != 0));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (n > 300)
+            //    Console.WriteLine((double)(b / c));
+            //else
+            //    Console.WriteLine((double)(a / c));
+
+            //58
+            //int n;
+            //char f = 'b';
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (a + b < 5)
+            //    f = 'a';
+            //Console.WriteLine(f);
+
+            //59
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100 && n % 10 != 0));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (a > b && a > c)
+            //{
+            //    if (b > c)
+            //    {
+            //        Console.WriteLine($"{c} {b} {a}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{b} {c} {a}");
+            //    }
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    if (a > c)
+            //    {
+            //        Console.WriteLine($"{c} {a} {b}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{a} {c} {b}");
+            //    }
+            //}
+            //else
+            //{
+            //    if (a > b)
+            //    {
+            //        Console.WriteLine($"{b} {a} {c}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{a} {b} {c}");
+            //    }
+            //}
+
+            //60
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel eranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 999 && n >= 100));
+
+            //int a = n / 100;
+            //int b = n % 100 / 10;
+            //int c = n % 10;
+
+            //if (a > b && a > c)
+            //{
+            //    if (b > c)
+            //    {
+            //        Console.WriteLine($"{a} {b} {c}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{a} {c} {b}");
+            //    }
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    if (a > c)
+            //    {
+            //        Console.WriteLine($"{b} {a} {c}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{b} {c} {a}");
+            //    }
+            //}
+            //else
+            //{
+            //    if (a > b)
+            //    {
+            //        Console.WriteLine($"{c} {a} {b}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{c} {b} {a}");
+            //    }
+            //}
+
+            //61
+            //int n;
+            //bool t = false;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (a + b == c + d)
+            //{
+            //    t = true;
+            //}
+            //Console.WriteLine(t);
+
+            //62
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000 && ((n % 10) + (n % 1000 / 100) != 0)));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (n < 5000)
+            //{
+            //    Console.WriteLine((double)n / (d + b));
+            //}
+            //else
+            //{
+            //    Console.WriteLine((double)n / (a + c));
+            //}
+
+            //63
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (a == 1 || b == 1 || c == 1 || d == 1)
+            //{
+            //    Console.WriteLine(1);
+            //}else
+            //    Console.WriteLine(0);
+
+            //64
+            //int n;
+            //char y = 'd';
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (d + c == 5)
+            //{
+            //    y = 's';
+            //}
+            //Console.WriteLine(y);
+
+            //65
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (c * d == 12)
+            //{
+            //    Console.WriteLine("y=12");
+            //}else
+            //    Console.WriteLine("y=0");
+
+            //66
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (a == 4 || d == 4)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+            //67
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (n == Math.Pow(a + b + c + d, 2))
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //    Console.WriteLine("NO");
+
+            //68
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (d > c)
+            //{
+            //    Console.WriteLine(d * b);
+            //}
+            //else
+            //    Console.WriteLine(1);
+
+            //69
+            //int n, y = 0;
+            //do
+            //{
+            //    Console.WriteLine("mutqagrel qaranish tiv");
+            //    n = int.Parse(Console.ReadLine());
+            //} while (!(n <= 9999 && n >= 1000));
+
+            //int a = n / 1000;
+            //int b = n % 1000 / 100;
+            //int c = n % 100 / 10;
+            //int d = n % 10;
+
+            //if (a + b + c + d > 20)
+            //    y = 1;
+            //Console.WriteLine(y);
+
+            //70
+            int n, y = 1;
+            do
+            {
+                Console.WriteLine("mutqagrel qaranish tiv");
+                n = int.Parse(Console.ReadLine());
+            } while (!(n <= 9999 && n >= 1000));
+
+            int a = n / 1000;
+            int b = n % 1000 / 100;
+            int c = n % 100 / 10;
+            int d = n % 10;
+
+            if (a * b * c * d > 200)
+                y = 0;
+            Console.WriteLine(y);
+        }
+    }
+}
